e8763e16b9fa2956addf242383a53916b1adf7e8
DeNhAiKal <115686752+DeNhAiKal@users.noreply.github.com>
2025-02-04 13:22:09 +0800

    Added stock level

--- app.py
+++ app.py
@@ -420,5 +420,5 @@ def error404():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    # app.run(host='0.0.0.0', port=5000, debug=True)
+    #app.run(debug=True)
+     app.run(host='0.0.0.0', port=5000, debug=True)
