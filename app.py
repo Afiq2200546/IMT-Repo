9545baeb03c6b5a6e3ad52c4f0ab71402ce0b36e
Afiq2200546 <115019647+Afiq2200546@users.noreply.github.com>
2025-02-11 12:40:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import pymongo
 from flask import (
     Flask,
     render_template,
@@ -8,11 +7,8 @@ from flask import (
     session,
     flash,
     g,
-    jsonify,
 )
-from werkzeug.security import generate_password_hash, check_password_hash
-from datetime import datetime
-import base64
+from werkzeug.security import generate_password_hash
 
 from dotenv import load_dotenv
 import os
